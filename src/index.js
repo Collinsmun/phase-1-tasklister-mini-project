a06f3a946d9b47e0b870257db653eca835197900
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-14 11:42:17 +0300

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let formNew = document.querySelector(`form`)
+  formNew.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+  // get value from input field
+  let name = document.getElementById('new-task-description').value;                                                     
+  handleToDo(name)
+  console.log(e)
+  formNew.reset()
+  
+  });
+  
+  });
+  function handleToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  // deleted
+  btn.addEventListener("click" ,deleteTask);
+
+  btn.textContent = 'x' ;
+  p.innerHTML= todo;
+  // p.textContent = todo;
+   p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#list').appendChild(p);
+  }
+  
+
+  function deleteTask(e){
+    e.target.parentNode.remove();
+  }
